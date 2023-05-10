6f0b8bfe15d603ff22d60ae0e1edb3a271733cb4
Vanessa Campioni <vanessacrtsilva@gmail.com>
2023-05-09 21:47:01 -0300

    starting project

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+    <Head>
+      <title>Portfolio</title>
+    </Head>
+    <main className="font-titleFont">New Portfolio Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, adipisci.</main>
+    </>
+  )
+}
