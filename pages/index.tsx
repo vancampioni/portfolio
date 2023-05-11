d9ddee248e1810395bb39ad84c960a7c18d743d6
Vanessa Campioni <vanessacrtsilva@gmail.com>
2023-05-11 20:05:24 -0300

    feat: add eslint and prettier configurations to the code

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,13 @@ import Head from "next/head";
 export default function Home() {
   return (
     <>
-    <Head>
-      <title>Portfolio</title>
-    </Head>
-    <main className="font-titleFont">New Portfolio Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, adipisci.</main>
+      <Head>
+        <title>Portfolio</title>
+      </Head>
+      <main className="font-titleFont">
+        New Portfolio Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Impedit, adipisci.
+      </main>
     </>
-  )
+  );
 }
